10912a4447c2a54b8a577a11c55ae1b166b137aa
bouncyhead1 <104355151+bouncyhead1@users.noreply.github.com>
2022-08-06 22:49:10 +0300

    adding usertypeone

--- friend-finder/src/components/MainContent/Main.jsx
+++ friend-finder/src/components/MainContent/Main.jsx
@@ -2,40 +2,39 @@ import React from "react";
 import s from './Main.module.css'
 
 const Main = () => {
-    return (
-        <div className="maincontent">
-        <div className="createpost">
-            <div className={s.createpost}>
-                <input type="Write what you wish" />
-                <button>*</button>
-                <button>*</button>
-                <button>*</button>
-                <button>publish</button>
+    return (       <>
+            <div className="createpost">
+                <div className={s.createpost}>
+                    <input type="Write what you wish" />
+                    <button>*</button>
+                    <button>*</button>
+                    <button>*</button>
+                    <button>publish</button>
+                </div>
             </div>
-            </div>
-        <div className="postcontainer">
-            <div className={s.postcontainer}>
-                
+            <div className="postcontainer">
+                <div className={s.postcontainer}>
+
                     <img src="#" alt="#" />
                     {/* image */}
                     <div className={s.postcontent}>
-                    <div className={s.post}>
-                        <div className={s.user}>
-                            {/* AVATAR */}<img src="" alt="" />
-                            <div>name</div>
+                        <div className={s.post}>
+                            <div className={s.user}>
+                                {/* AVATAR */}<img src="" alt="" />
+                                <div>name</div>
 
-                            <div>published</div>
-                            <button>Like</button>
-                            <button>Dislike</button>
+                                <div>published</div>
+                                <button>Like</button>
+                                <button>Dislike</button>
+                            </div>
+
                         </div>
-                    
-                    </div>
-                    <div className={s.commentaryy}>Comment</div>
+                        <div className={s.commentaryy}>Comment</div>
                     </div>
                 </div>
-            
-                </div>
-                </div>
+
+            </div>
+</>
     );
 }
 export default Main;
---
+++ friend-finder/src/components/usertypes/usertypes.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import s from './usertypes.module.css'
+
+const usertypeone = () => {
+    return (
+        <>
+        <div className="userfirst">
+            <div className="user">
+                <img src="" alt="" />
+                <div className="name">
+                    <a href="3" className="name">
+                        ${NAME}
+                    </a>
+                </div>
+            </div>
+        </div>
+        </>
+    )
+}
+export default usertypeone
